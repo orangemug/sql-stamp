b0f2f5f31e197cb102ce80705ca8359c8eda445a
orangemug <info@orangemug.co.uk>
2015-05-13 19:12:56 +0100

    Added templates to CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
 var fs       = require("fs");
+var path     = require("path");
+var glob     = require("glob");
 var lodash   = require("lodash");
+var Bluebird = require("bluebird");
 var sqlStamp = require("../");
+
+fs = Bluebird.promisifyAll(fs);
 
 var argv = require('yargs')
   .usage('Usage: $0 [args] [sql-file] [args-file]')
+  .version(function() {
+    return require('../package').version
+  })
   .example("$0 --name foo ./path/to.sql ./path/data.json")
+  .string("template")
+  .describe("template", "Template base location (defaults dir of [sql-file])")
   .demand(1)
   .argv;
 
 var dataFilepath = argv._[1];
 var sqlFilepath  = argv._[0];
+var sqlBasename  = path.dirname(sqlFilepath);
 var sqlTemplate  = fs.readFileSync(process.cwd()+"/"+sqlFilepath).toString();
 
 var data = {};
@@ -24,12 +35,29 @@ for(var k in argv) {
   }
 }
 
-function getTemplates(path, done) {
-  // TODO
-  done(undefined, {});
+// Fetch the templates
+function getTemplates(dirpath, done) {
+  glob(dirpath+"/**/*.sql", {}, function(err, files) {
+    if(err) {
+      done(err);
+      return;
+    }
+
+    var out = {};
+    var data = Bluebird.each(files, function(filepath) {
+      var relpath = "./"+path.relative(dirpath, filepath);
+      return fs.readFileAsync(filepath).then(function(data) {
+        out[relpath] = data.toString();
+      });
+    }).then(function(data) {
+      done(undefined, out);
+    }).catch(function(err) {
+      done(err);
+    })
+  });
 }
 
-getTemplates("/path", function(err, templates) {
+getTemplates(sqlBasename, function(err, templates) {
   if(err) {
     throw err;
   }
