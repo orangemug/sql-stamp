86b92420144c08c0717b4f731659e4b036118769
orangemug <info@orangemug.co.uk>
2015-05-22 12:10:38 +0100

    New pre processor.

--- index.js
+++ index.js
@@ -2,30 +2,51 @@ var util      = require("./lib/util");
 var operators = require("./lib/operators");
 
 
+function getBody(fn) {
+  var regex = /^function[ ]*[^(]*\(([^)]*)\)[ ]*{((?:[\n\r]|.)*)}$/;
+  var matches = fn.toString().match(regex);
+
+  return {
+    args: matches[1].split(",").map(util.chomp),
+    body: matches[2]
+  }
+}
+
+
+function runner(template, ctx) {
+  return template
+    .map(function(part, idx) {
+      if(part.fn) {
+        return part.fn(ctx);
+      } else {
+        return part.sql;
+      }
+    })
+    .join("");
+}
+
 function genTemplateFn(sqlRaw, templates) {
+  var parts = [];
   var out = sqlRaw.replace(/([^}]*)(?:\{([>?!]?)([^}]*)\})([^{]*)/g, function(match, pre, type, args, post, offset) {
     var operatorArgs = args.split(",")
       .map(util.chomp)
       .map(util.removeQuotes);
 
-    pre  = util.escapeString(pre);
-    post = util.escapeString(post);
+    parts.push({sql: pre});
 
-    var out;
-    if(offset > 0) {
-      out = "+";    
-    } else {
-      out = "return ";
-    }
-    out += "\""+pre+"\"";
-    
-    out += "+operators[\""+type+"\"](ctx, "+operatorArgs.map(util.addQuotes).join(",")+")";
-    out += "+\""+post+"\"";
-    return out;
+    parts.push({
+      sql: "{}",
+      fn: function(ctx) {
+        return operators[type](ctx, operatorArgs);
+      }
+    });
+
+    parts.push({sql: post});
   })
 
-  var fn = new Function("operators", "ctx", "data", out);
-  return fn.bind(null, operators);
+  var fnBody = getBody(runner)
+  var fn = new Function(fnBody.args, fnBody.body).bind(null, parts);
+  return fn;
 }
 
 function sqlStamp(_templates) {
@@ -37,7 +58,7 @@ function sqlStamp(_templates) {
   }
 
   return function hdl(key, data) {
-    key = util.chomp(key);
+    key  = util.chomp(key);
     data = data || {};
 
     var ctx = {
