f5930305927070948ce59b903a33b609a47936f1
orangemug <info@orangemug.co.uk>
2015-06-12 20:06:10 +0100

    Tidy initial parser

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var Bluebird = require("bluebird");
 var util     = require("./lib/util");
 var parser   = require("./lib/parser");
 
+fs = Bluebird.promisifyAll(fs);
+
 /**
  * Initialize a SQL templater
  * @param {Array} files
@@ -18,31 +20,19 @@ module.exports = function(files, opts, callback) {
   var tasks = [];
 
   // Generate the templates
-  var parsedTemplates = {};
+  var templatesToProcess = {};
   files.forEach(function(filepath) {
     filepath = path.resolve(filepath);
 
-    tasks.push(new Bluebird(function(resolve, reject) {
-      fs.readFile(filepath, function(err, data) {
-        if(err) {
-          reject(err);
-        } else {
-          parser(data.toString(), opts)
-            .then(function(out) {
-              parsedTemplates[filepath] = out
-              resolve();
-            })
-            .catch(function(err) {
-              reject(err);
-            });
-        }
+    templatesToProcess[filepath] = fs.readFileAsync(filepath)
+      .then(function(data) {
+        return parser(data.toString(), opts);
       });
-    }));
   });
 
   return Bluebird
-    .all(tasks)
-    .then(function() {
+    .props(templatesToProcess)
+    .then(function(_templates) {
       // Return a template runner
       return function hdl(key, data) {
         key  = path.resolve(util.chomp(key));
@@ -51,13 +41,13 @@ module.exports = function(files, opts, callback) {
         var ctx = {
           path: key,
           self: hdl,
-          templates: parsedTemplates,
+          templates: _templates,
           data: data,
           args: []
         };
 
         return {
-          sql: parsedTemplates[key](ctx),
+          sql: _templates[key](ctx),
           args: ctx.args
         };
       }
