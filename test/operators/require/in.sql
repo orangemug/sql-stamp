98b69f3a70dc873c4b974ea01906d2207b71eb02
orangemug <info@orangemug.co.uk>
2015-06-12 19:25:06 +0100

    Added require indent fixes

--- test/operators/require/in.sql
+++ test/operators/require/in.sql
@@ -1,3 +1,7 @@
-WITH some_cte AS ({> ./in_sub.sql}),
-some_other_cte AS ({> ./in_sub_nested.sql, nested_var})
+WITH some_cte AS (
+  {> ./in_sub.sql}
+),
+some_other_cte AS (
+  {> ./in_sub_nested.sql, nested_var}
+)
 select * from some_cte, some_other_cte
--- test/operators/require/out.sql
+++ test/operators/require/out.sql
@@ -1,3 +1,17 @@
-WITH some_cte AS (select * from account where name = ?),
-some_other_cte AS (select * from nested where foo = "bar")
+WITH some_cte AS (
+  select
+    *
+  from
+    account
+  where
+    name = ?
+),
+some_other_cte AS (
+  select
+    *
+  from
+    nested
+  where
+    foo = "bar"
+)
 select * from some_cte, some_other_cte
