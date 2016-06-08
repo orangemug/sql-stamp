30f16347518427ff54038ebefa315a04447d0682
orangemug <info@orangemug.co.uk>
2016-06-08 07:02:58 +0100

    Added in new param passing syntax (#9)

--- test/operators/require/in.sql
+++ test/operators/require/in.sql
@@ -2,6 +2,6 @@ WITH some_cte AS (
   {> ./in_sub.sql}
 ),
 some_other_cte AS (
-  {> ./in_sub_nested.sql, nested_var}
+  {> ./in_sub_nested.sql, ...nested_var}
 )
 select * from some_cte, some_other_cte
