0713e9f13b79cc21a263ca3e07182894a901b9ae
Oliver Brooks <oli@pearlshare.com>
2016-07-06 11:23:08 +0100

    eslint code

--- test/benchmark/no-pre-compile.js
+++ test/benchmark/no-pre-compile.js
@@ -1,12 +1,12 @@
 var Benchmark = require("benchmark");
 var sqlStamp  = require("../../");
 
-var bench = new Benchmark('[sql-stamp] no-pre-compile', {
+var bench = new Benchmark("[sql-stamp] no-pre-compile", {
   // a flag to indicate the benchmark is deferred
-  'defer': true,
+  "defer": true,
 
   // benchmark test function
-  'fn': function(deferred) {
+  "fn": function(deferred) {
     sqlStamp([
       __dirname+"/../functional/example.sql",
       __dirname+"/../functional/friends.sql",
@@ -20,20 +20,19 @@ var bench = new Benchmark('[sql-stamp] no-pre-compile', {
         filterVal: "dev"
       });
       deferred.resolve();
-    })
+    });
   }
 });
 
-bench.on('complete', function(event) {
+bench.on("complete", function(event) {
   console.log(String(event.target));
 });
 
-bench.on('error', function(err) {
+bench.on("error", function(err) {
   console.error(err);
 });
 
 // Kick off the benchmark
 bench.run({
-  'async': false
+  "async": false
 });
-
